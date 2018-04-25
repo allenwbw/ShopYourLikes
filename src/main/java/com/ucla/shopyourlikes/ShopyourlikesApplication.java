06d778ba0565933575160655a5c5ca4bf8991059
argold97 <argold97@gmail.com>
2018-04-24 17:23:00 -0700

    Initial commit

---
+++ src/main/java/com/ucla/shopyourlikes/ShopyourlikesApplication.java
@@ -0,0 +1,12 @@
+package com.ucla.shopyourlikes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopyourlikesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopyourlikesApplication.class, args);
+	}
+}
