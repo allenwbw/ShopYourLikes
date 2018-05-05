a4b0ad35007e3b4c668f3bda6d5bc6d417fc0a8c
Alex <argold97@gmail.com>
2018-05-05 05:29:22 -0700

    Final fix

--- src/main/java/com/ucla/shopyourlikes/security/JwtAuthenticationFilter.java
+++ src/main/java/com/ucla/shopyourlikes/security/JwtAuthenticationFilter.java
@@ -1,10 +1,12 @@
-package com.ucla.ShopYourLikes.security;
+package com.ucla.shopyourlikes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
@@ -13,13 +15,16 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
     @Autowired
     private JwtTokenProvider tokenProvider;
 
@@ -27,7 +32,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-        throws ServletException, IOException {
+            throws ServletException, IOException {
         try {
             String jwt = getJwtFromRequest(request);
             if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
@@ -46,11 +51,17 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     private String getJwtFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7, bearerToken.length());
+        Cookie[] cookies = request.getCookies();
+        if(cookies == null)
+            return null;
+        for (Cookie c : cookies) {
+            if(c.getName().equals("Authorization")) {
+                String bearerToken = c.getValue();
+                if (StringUtils.hasText(bearerToken)) {
+                    return bearerToken;
+                }
+            }
         }
-
         return null;
     }
 }
