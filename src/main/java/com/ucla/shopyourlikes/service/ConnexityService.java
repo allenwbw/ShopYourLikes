a2768ba4e14d7250032db790b9f5c458ee6b0f59
robertposada <rep2@stmarys-ca.edu>
2018-05-10 18:30:44 -0700

    Added Connexity Service

---
+++ src/main/java/com/ucla/shopyourlikes/service/ConnexityService.java
@@ -0,0 +1,51 @@
+package com.ucla.shopyourlikes.service;
+
+import com.ucla.shopyourlikes.model.User;
+import com.ucla.shopyourlikes.payload.GenerateLinkResponse;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ConnexityService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConnexityService.class);
+
+    public List<GenerateLinkResponse> connexityRequest(User user, List<String> urls) {
+
+        List<GenerateLinkResponse> sylLinks = new ArrayList<>();
+
+        String publisherid = user.getUserId().toString();
+        String apiKey = user.getApiKey();
+
+        String baseUrl = "http://api.shopyourlikes.com/api/link/generate?url=";
+
+        RestTemplate restTemplate = new RestTemplate();
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+
+        //Add the Jackson Message converter
+        messageConverters.add(new MappingJackson2HttpMessageConverter());
+        //Add the message converters to the restTemplate
+        restTemplate.setMessageConverters(messageConverters);
+
+        for (String url : urls){
+            GenerateLinkResponse response = new GenerateLinkResponse();
+            String reqUrl = baseUrl+encodeValue(url)+"&publisherId="+publisherid+"&apiKey="+apiKey;
+            try {
+                response = restTemplate.getForObject(reqUrl, GenerateLinkResponse.class);
+            } catch (HttpClientErrorException ce) {
+                logger.error("Fail to contact SYL server",ce);
+            }
+            sylLinks.add(response);
+        }
+
+        return sylLinks;
+    }
+}
