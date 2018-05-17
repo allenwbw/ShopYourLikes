bc95c3455496c4b9aacc8d612c6b34558868fba5
Qinhaoxu <qinhaoxu@yahoo.com>
2018-05-17 13:12:39 -0700

    linkservice testcases

--- src/test/java/com/ucla/shopyourlikes/service/testLinksService.java
+++ src/test/java/com/ucla/shopyourlikes/service/testLinksService.java
@@ -1,12 +1,28 @@
 package com.ucla.shopyourlikes.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+
+import com.ucla.shopyourlikes.exception.BadRequestException;
+import com.ucla.shopyourlikes.model.User;
 import jdk.internal.dynalink.linker.LinkerServices;
+import org.assertj.core.api.Assertions;
+import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.*;
 
 
 // TODO: will add more testcase
@@ -24,5 +40,38 @@ public class testLinksService {
     public void testMockCreation() {
         assertNotNull(linksService);
     }
+
+    @Test
+    public void testGetAllLinks_goodcase () {
+        User user = new User();
+        user.setUserId(628626);
+        user.setApiKey("7438a399d83eceffef422e5563a94401");
+        linksService.getAllLinks(user,-1,1);
+    }
+
+    @Test
+    public void testGetAllLinksThrowBadRequestException() {
+        User user = new User();
+        user.setUserId(628626);
+        when(linksService.getAllLinks(user,-1,-1)).thenThrow(new BadRequestException(""));
+    }
+
+    @Test
+    public void testCreateLinks_goodcase() {
+
+        User user = new User();
+        user.setUserId(628626);
+        user.setApiKey("7438a399d83eceffef422e5563a94401");
+
+        List<String> urls =  new ArrayList<>();
+        urls.add("www.macys.com");
+
+        linksService.createLinks(user,urls);
+
+    }
+
+
+
+
 
 }
