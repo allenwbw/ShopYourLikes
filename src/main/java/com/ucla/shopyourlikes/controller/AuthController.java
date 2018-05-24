758b4343fcd3b651e14a71dafe1d92eb1555613c
Qinhaoxu <qinhaoxu@yahoo.com>
2018-05-24 09:48:27 -0700

    added auth controller input validation and testcase

--- src/main/java/com/ucla/shopyourlikes/controller/AuthController.java
+++ src/main/java/com/ucla/shopyourlikes/controller/AuthController.java
@@ -1,12 +1,14 @@
 package com.ucla.shopyourlikes.controller;
 
 
+import com.google.common.base.Strings;
 import com.ucla.shopyourlikes.payload.external.JwtAuthenticationResponse;
 import com.ucla.shopyourlikes.payload.external.LoginRequest;
 import com.ucla.shopyourlikes.repository.RoleRepository;
 import com.ucla.shopyourlikes.repository.UserRepository;
 import com.ucla.shopyourlikes.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -23,19 +25,24 @@ import javax.validation.Valid;
 @RequestMapping("/api/auth")
 public class AuthController {
     @Autowired
-    AuthenticationManager authenticationManager;
+    protected AuthenticationManager authenticationManager;
 
     @Autowired
-    UserRepository userRepository;
+    protected UserRepository userRepository;
 
     @Autowired
-    RoleRepository roleRepository;
+    protected RoleRepository roleRepository;
 
     @Autowired
-    JwtTokenProvider tokenProvider;
+    protected JwtTokenProvider tokenProvider;
 
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest){
+
+        if(Strings.isNullOrEmpty(loginRequest.getUserId()) || Strings.isNullOrEmpty(loginRequest.getApiKey())){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
         Authentication authentication = authenticationManager
                 .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getApiKey()));
 
--- src/test/java/com/ucla/shopyourlikes/controller/testAuthController.java
+++ src/test/java/com/ucla/shopyourlikes/controller/testAuthController.java
@@ -1,22 +1,79 @@
 package com.ucla.shopyourlikes.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import com.ucla.shopyourlikes.payload.external.LoginRequest;
+import com.ucla.shopyourlikes.repository.RoleRepository;
+import com.ucla.shopyourlikes.repository.UserRepository;
+import com.ucla.shopyourlikes.security.JwtTokenProvider;
+
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+
 
 // TODO: will add more testcase
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class testAuthController {
 
-    @Autowired
+    @Mock
+    private AuthenticationManager authenticationManager;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private RoleRepository roleRepository;
+
+    @Mock
+    private JwtTokenProvider tokenProvider;
+
     private AuthController authController;
 
-    @Test
-    public void contextLoads() throws Exception {
-        assertThat(authController).isNotNull();
+    @Before
+    public void setupMock() {
+        MockitoAnnotations.initMocks(this);
+        authController = new AuthController();
+        authController.authenticationManager= authenticationManager;
+        authController.userRepository = userRepository;
+        authController.roleRepository = roleRepository;
+        authController.tokenProvider = tokenProvider;
+
     }
+
+    @Test
+    public void authenticateUser_ok()  {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setApiKey("ucla");
+        loginRequest.setUserId("bruin");
+        ResponseEntity<?> responseEntity = authController.authenticateUser(loginRequest);
+
+        assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void authenticateUser_BAD_REQUEST_emptyApiKey()  {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setApiKey("");
+        loginRequest.setUserId("bruin");
+
+        ResponseEntity<?> responseEntity = authController.authenticateUser(loginRequest);
+
+        assertEquals(HttpStatus.BAD_REQUEST,responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void authenticateUser_BAD_REQUEST_emptyUserId()  {
+        LoginRequest loginRequest = new LoginRequest();
+        loginRequest.setApiKey("ucla");
+        loginRequest.setUserId("");
+
+        ResponseEntity<?> responseEntity = authController.authenticateUser(loginRequest);
+
+        assertEquals(HttpStatus.BAD_REQUEST,responseEntity.getStatusCode());
+    }
+
 }
