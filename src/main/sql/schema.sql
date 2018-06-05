c65f371e520d2ef3ddb83bd4e31b15c9373c1891
DEZHI-HUANG <38392709+DEZHI-HUANG@users.noreply.github.com>
2018-06-05 15:28:10 -0700

    Update schema.sql

--- src/main/sql/schema.sql
+++ src/main/sql/schema.sql
@@ -12,16 +12,19 @@ CREATE TABLE links (
   creation_date DATETIME NOT NULL,
   redirects INT NOT NULL,
   earnings FLOAT UNSIGNED NOT NULL,
-  epc FLOAT UNSIGNED NOT NULL,
+  ecpc FLOAT UNSIGNED NOT NULL,
   ig_image_url VARCHAR(30),
   original_url VARCHAR(30) NOT NULL,
   merchant_id INT UNSIGNED NOT NULL,
+  merchant_name VARCHAR(50)
   name VARCHAR(30),
   PRIMARY KEY (user_id, hash)
 );
 
 CREATE TABLE merchants (
   merchant_id INT UNSIGNED NOT NULL,
-  name VARCHAR(30),
+  merchant_domain VARCHAR(50),
+  merchant_tld VARCHAR(5),
+  merchant_name VARCHAR(50),
   PRIMARY KEY (merchant_id)
 );
